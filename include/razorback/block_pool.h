d2cd67f3d45d03c4c628f2f2c2a456a99d1d1301
Tom Judge <157825077+tj-cisco@users.noreply.github.com>
2026-03-26 20:30:52 -0400

    Differentiate no-type submission failures (#11)
    * Differentiate no-type submission failures
    
    * Update doc

--- include/razorback/block_pool.h
+++ include/razorback/block_pool.h
@@ -41,6 +41,7 @@ extern "C" {
 #define BLOCK_POOL_STATUS_PAGED                 0x00000011 ///< Block is currently paging out
 #define BLOCK_POOL_STATUS_DESTROY               0x00000012 ///< Destroy this item
 #define BLOCK_POOL_STATUS_ERROR                 0x00000020 ///< Terminal submission error
+#define BLOCK_POOL_STATUS_NO_TYPE               0x00000021 ///< Terminal submission failure due to missing data type
 
 #define BLOCK_POOL_FLAG_MASK                    0xFF000000 ///< Mask for Flags bits
 #define BLOCK_POOL_FLAG_MAY_REUSE               0x01000000 ///< This item should not be fully removed from the pool
